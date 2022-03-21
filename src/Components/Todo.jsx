376c3fcf49ba88db8f6d154e8555db4be28989df
vikalpvarshney1 <vikalpvarshney012@gmail.com>
2022-03-21 21:33:04 +0530

    b

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Todo = () => {
     const [inputValue , setInputValue] =React.useState("");
-    // const [inputValue2 , setInputValue2] =React.useState("");
+    const [inputValue2 , setInputValue2] =React.useState("");
+    const [inputValue3 , setInputValue3] =React.useState("");
+    const [inputValue4 , setInputValue4] =React.useState("");
     const [todos , setTodos] =React.useState([])
  
   
@@ -24,7 +26,9 @@ const Todo = () => {
       console.log(inputValue)
       const payload ={
           title: inputValue,
-          
+          name: inputValue2,
+          gender:inputValue3,
+          department:inputValue4,
           status: false
       };
 
@@ -47,19 +51,33 @@ const Todo = () => {
     <div>
 
 <input placeholder='Add Todos' value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-{/* <input placeholder='Add Todos' value={inputValue2} onChange={(e) => setInputValue2(e.target.value)}/> */}
+<br></br>
+<input placeholder='Add Todos' value={inputValue2} onChange={(e) => setInputValue2(e.target.value)}/>
+<br></br>
+<input placeholder='Add Todos' value={inputValue3} onChange={(e) => setInputValue3(e.target.value)}/>
+<br></br>
+<input placeholder='Add Todos' value={inputValue4} onChange={(e) => setInputValue4(e.target.value)}/>
+<br></br>
 <button onClick={handleAdd}>ADD</button>
+<center>
 {
     todos.map((item)=> {
-        return   <div>{item.title}
-
-        </div>;
+        return   <h5>{item.title}
+        <br></br>
+{item.name}
+<br></br>
+{item.gender}
+<br></br>
+{item.department}
+        </h5>;
        
    
     })
 }
+</center>
     </div>
   )
 }
+
 
 export  {Todo};
